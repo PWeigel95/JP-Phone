58f87e251f3c441f81c2233934c546497a1dda83
Philipp Weigel <wi20b018@technikum-wien.at>
2022-05-03 19:58:47 +0200

    update

---
+++ config/dbaccess.php
@@ -0,0 +1,9 @@
+<?php
+
+//beinhaltet die Login Daten zur Datenbank
+$servername = "localhost";
+$username = "JP";
+$password = "password";
+$dbname = "jp-phone";
+
+?>
--- logic/requestHandler.php
+++ logic/requestHandler.php
@@ -1,6 +1,19 @@
 <?php 
 
-function createUser(){ } 
+//benötigt für den Datenbankzugriff
+require_once "../config/dbaccess.php";
 
+
+function createUser(){ 
+    /*
+    $db_obj = new mysqli($servername, $username, $password, $dbname);
+
+    $sql = "INSERT INTO `users`(`username`, `password`, `email`, `role_id`,`u_status`, `anrede`,`fname`,`lname`) VALUES (?, ?, ?, ?, ?,?,?,?)";
+
+    $stmt = $db_obj->prepare($sql);
+
+    $stmt->bind_param("sssiisss", $uname, $pass, $mail, $role_id, $u_status, $anrede, $fname, $lname);
+    */
+} 
 
 ?>
