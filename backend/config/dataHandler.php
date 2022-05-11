7d2516bf8f6d79981247db2fe8d015307165823c
Philipp Weigel <wi20b018@technikum-wien.at>
2022-05-11 19:41:38 +0200

    updated

--- backend/config/dataHandler.php
+++ backend/config/dataHandler.php
@@ -1,19 +1,19 @@
 <?php
 
-include ("dbaccess.php");
-$db_obj = new mysqli($servername, $username, $password, $dbname);
-
 class Datahandler{
 
     public function createUser($userdata){
-        //$sql = "INSERT INTO `users`(`username`, `password`, `email`, `role_id`,`u_status`, `anrede`,`fname`,`lname`) VALUES (?, ?, ?, ?, ?,?,?,?)";
-        //$stmt = $db_obj->prepare($sql);
+        include ("dbaccess.php");
+        $db_obj = new mysqli($servername, $username, $password, $dbname);
+
+        $sql = "INSERT INTO `users`(`username`, `password`, `email`, `role_id`,`u_status`, `anrede`,`fname`,`lname`) VALUES (?, ?, ?, ?, ?,?,?,?)";
+        $stmt = $db_obj->prepare($sql);
         //$stmt->bind_param("sssiisss", $uname, $pass, $mail, $role_id, $u_status, $anrede, $fname, $lname);
 
         //close the statement
-        //$stmt->close();
+        $stmt->close();
         //close the connection
-        //$db_obj->close();
+        $db_obj->close();
     }
     
 }
