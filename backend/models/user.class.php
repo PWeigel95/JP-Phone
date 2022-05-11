f6c1cf23ec756b62d9cee75d8536de7e853ef463
Philipp Weigel <wi20b018@technikum-wien.at>
2022-05-11 11:52:39 +0200

    updated user.class

--- backend/models/user.class.php
+++ backend/models/user.class.php
@@ -1,6 +1,37 @@
 <?php
 class User {
-    function get($id) {
-    //
-    }
+    public $id;
+    public $anrede;
+    public $vorname;
+    public $nachname;
+    public $adresse;
+    public $plz;
+    public $ort;
+    public $email;
+    public $benutzername;
+    public $passwort;
+    public $zahlungsinformation;
+    public $role_id;
+    public $user_status;
+    public $erstellungsdatum;
+
+    function __construct($id, $anrede, $vorname, $nachname, $adresse, $plz, $ort,$email,
+    $benutzername, $passwort, $zahlungsinformation, $role_id,  $user_status, $erstellungsdatum) {
+        $this->id = $id;
+        $this->anrede = $anrede;
+        $this->vorname=$vorname;
+        $this->nachname=$nachname;
+        $this->adresse=$adresse;
+        $this->departmentplz=$plz;
+        $this->ort=$ort;
+        $this->email=$email;
+        $this->benutzername=$benutzername;
+        $this->passwort=$passwort;
+        $this->zahlungsinformation=$zahlungsinformation;
+        $this->role_id=$role_id;
+        $this->user_status=$user_status;
+        $this->erstellungsdatum=$erstellungsdatum;
+        
+      }
+
 }
