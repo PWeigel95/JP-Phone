39696765b493c85367b8ca2a20a70922a9886f02
Philipp Weigel <wi20b018@technikum-wien.at>
2022-06-08 23:55:54 +0200

    create admin.js file

---
+++ js/admin.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    $("#v-pills-editProducts-tab").on("click", function() {
+        loadEditProductPage();
+
+    })
+
+
+    $("#v-pills-editCustomers-tab").on("click", function() {
+        loadEditCustomerPage();
+
+    })
+
+    $("#v-pills-editVouchers-tab").on("click", function() {
+        loadEditVoucherPage();
+
+    })
+
+    function loadEditProductPage() {
+
+    }
+
+    function loadEditCustomerPage() {
+
+    }
+
+    function loadEditVoucherPage() {
+
+    }
+
+
+
+
+})
